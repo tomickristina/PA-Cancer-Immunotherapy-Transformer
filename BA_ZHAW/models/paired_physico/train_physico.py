3df4fe44b5c19a836ef6c3bb0cf3a426f5d5c5aa
oscario-20 <tomickri@students.zhaw.ch>
2024-11-03 09:50:11 +0000

    model adjustments

--- BA_ZHAW/models/paired_physico/train_physico.py
+++ BA_ZHAW/models/paired_physico/train_physico.py
@@ -19,7 +19,7 @@ torch.manual_seed(42)
 # ---------------------------------------------------------------------------------
 MODEL_NAME = "PhysicoModel"
 EMBEDDING_SIZE = 1024
-BATCH_SIZE = 128
+BATCH_SIZE = 64 #128
 EPOCHS = 250
 # IMPORTANT: keep NUM_WORKERS = 0!
 NUM_WORKERS = 0
@@ -147,12 +147,9 @@ def main():
     experiment_name = f"Experiment - {MODEL_NAME}"
     load_dotenv()
     PROJECT_NAME = os.getenv("MAIN_PROJECT_NAME")
+    PROJECT_NAME = "dataset-allele"
     print(f"PROJECT_NAME: {PROJECT_NAME}")
-    #run = wandb.init(project=PROJECT_NAME, job_type=f"{experiment_name}", entity="ba-zhaw")
-    run = wandb.init(
-    project="test",
-    job_type="train",
-    entity="pa_cancerimmunotherapy")
+    run = wandb.init(project=PROJECT_NAME, job_type=f"{experiment_name}", entity="pa_cancerimmunotherapy")
     config = wandb.config
 
     # -----------------------------------------------------------------------------
