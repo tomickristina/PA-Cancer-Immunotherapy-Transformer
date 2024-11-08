f6cb8c7b0eea2d6b544c778c03467b09c8135292
oscario-20 <tomickri@students.zhaw.ch>
2024-11-08 08:54:06 +0000

    adjustments on datapipelines

--- BA_ZHAW/models/paired_vanilla/train_vanilla.py
+++ BA_ZHAW/models/paired_vanilla/train_vanilla.py
@@ -126,7 +126,7 @@ def get_embed_len(df, column_name):
 
 def main():
     precision = "allele" # or allele
-    embed_base_dir = f"../../data/embeddings/paired/{precision}"
+    embed_base_dir = f"../../data_10x/embeddings/paired/{precision}" #data_10x für 10x
     hyperparameter_tuning_with_WnB = False
 
     # -----------------------------------------------------------------------------
