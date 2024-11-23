bbb4a2124b1735059d41c06351ccdf6b2d817081
liselott321 <arina.frohofer@gmail.com>
2024-11-23 09:27:07 +0000

    Update train_vanilla.py in models2/paired_vanilla

--- BA_ZHAW/models2/paired_vanilla/train_vanilla.py
+++ BA_ZHAW/models2/paired_vanilla/train_vanilla.py
@@ -126,8 +126,9 @@ def get_embed_len(df, column_name):
 
 
 def main():
-    precision = "gene" # "allele or gene"
-    embed_base_dir = f"../../data_10x/embeddings/paired/{precision}"
+    precision = "allele" # "allele or gene"
+    #embed_base_dir = f"../../data_10x/embeddings/paired/{precision}" # 10x
+    embed_base_dir = f"../../data/embeddings/paired/{precision}" # BA
     hyperparameter_tuning_with_WnB = False
 
     # -----------------------------------------------------------------------------
@@ -137,7 +138,8 @@ def main():
     experiment_name = f"Experiment - {MODEL_NAME}"
     load_dotenv()
     PROJECT_NAME = os.getenv("MAIN_PROJECT_NAME")
-    PROJECT_NAME = f'dataset-{precision}_10x' 
+    # PROJECT_NAME = f'dataset-{precision}_10x' # 10x
+    PROJECT_NAME = f'dataset-{precision}' # BA
     print(f"PROJECT_NAME: {PROJECT_NAME}")
     run = wandb.init(project=PROJECT_NAME, job_type=f"{experiment_name}", entity="pa_cancerimmunotherapy")
     config = wandb.config
